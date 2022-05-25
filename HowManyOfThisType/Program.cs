c9df932fa71ec894c01ecd821de52cd1a44e668e
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-25 11:27:56 +0100

    Create list of object type, add add multiple instances to it

--- HowManyOfThisType/Program.cs
+++ HowManyOfThisType/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace HowManyOfThisType
@@ -7,16 +8,30 @@ namespace HowManyOfThisType
     {
         static void Main(string[] args)
         {
-            
+            List<object> objectList = new List<object>();
+
+            int intObj = 5;
+            string strObj = "coiso";
+            float dblObj = 7.5f;
+            char chObj = 'c';
+            bool boolObj = true;
+
+            objectList.Add(intObj);
+            objectList.Add(strObj);
+            objectList.Add(dblObj);
+            objectList.Add(chObj);
+            objectList.Add(boolObj);
         }
 
         private static int NumberOfItemsOfType<T>(object[] objects)
         {
+            int objectNum = 0;
+
             foreach (object o in objects)
             {
-                if (o is T) return true;
+                if (o is T) objectNum++;
             }
-            return false;
+            return objectNum;
         }
     }
 }
