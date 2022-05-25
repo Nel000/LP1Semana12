d3b5f7e7c04f05a30246f2b64eeba2a0a7e4b907
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-25 11:45:26 +0100

    Show amount for each object type in objectList

--- HowManyOfThisType/Program.cs
+++ HowManyOfThisType/Program.cs
@@ -8,22 +8,36 @@ namespace HowManyOfThisType
     {
         static void Main(string[] args)
         {
+            int objectNum;
+
             List<object> objectList = new List<object>();
 
-            int intObj = 5;
+            int intObj = 5, intObj2 = 7;
             string strObj = "coiso";
             float dblObj = 7.5f;
             char chObj = 'c';
             bool boolObj = true;
 
             objectList.Add(intObj);
+            objectList.Add(intObj2);
             objectList.Add(strObj);
             objectList.Add(dblObj);
             objectList.Add(chObj);
             objectList.Add(boolObj);
+
+            objectNum = NumberOfItemsOfType<int>(objectList);
+            Console.WriteLine($"Number of ints:     {objectNum}");
+            objectNum = NumberOfItemsOfType<string>(objectList);
+            Console.WriteLine($"Number of strings:  {objectNum}");
+            objectNum = NumberOfItemsOfType<float>(objectList);
+            Console.WriteLine($"Number of floats:   {objectNum}");
+            objectNum = NumberOfItemsOfType<char>(objectList);
+            Console.WriteLine($"Number of chars:    {objectNum}");
+            objectNum = NumberOfItemsOfType<bool>(objectList);
+            Console.WriteLine($"Number of bools:    {objectNum}");
         }
 
-        private static int NumberOfItemsOfType<T>(object[] objects)
+        private static int NumberOfItemsOfType<T>(List<object> objects)
         {
             int objectNum = 0;
 
