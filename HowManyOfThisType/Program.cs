86ada5c6946e4823cddb03958a9d37e24b1266c6
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-25 11:14:55 +0100

    Add method to get number of items of type

--- HowManyOfThisType/Program.cs
+++ HowManyOfThisType/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HowManyOfThisType
 {
@@ -7,6 +8,15 @@ namespace HowManyOfThisType
         static void Main(string[] args)
         {
             
+        }
+
+        private static int NumberOfItemsOfType<T>(object[] objects)
+        {
+            foreach (object o in objects)
+            {
+                if (o is T) return true;
+            }
+            return false;
         }
     }
 }
