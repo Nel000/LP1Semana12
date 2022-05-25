d655d9c1e93b6996e1c6e18f746e29b89ce24614
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-25 23:43:13 +0100

    Write user input directly on file

--- WriteStuff2/Program.cs
+++ WriteStuff2/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
 
 namespace WriteStuff2
 {
     class Program
     {
+        private static string filePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            "Test",
+            "input.txt");
+
         static void Main(string[] args)
         {
-            
+            StreamWriter sw = new StreamWriter(filePath);
+
+            string input;
+
+            Console.WriteLine("Write lines of text to add to a new file.");
+            Console.WriteLine("When done, leave an empty line to exit program.");
+
+            Console.WriteLine();
+
+            do
+            {
+                input = " ";
+
+                Console.Write("> ");
+
+                input = Console.ReadLine();
+
+                if (input != "")
+                    sw.WriteLine(input);
+            }
+            while (input != "");
+
+            sw.Close();
         }
     }
 }
