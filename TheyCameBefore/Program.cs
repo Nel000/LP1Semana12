8bb4dbb537f9075a0b8251f33dc0f88f783b57a0
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-25 19:41:09 +0100

    Test method with int and string types

--- TheyCameBefore/Program.cs
+++ TheyCameBefore/Program.cs
@@ -7,7 +7,34 @@ namespace TheyCameBefore
     {
         static void Main(string[] args)
         {
+            IEnumerable<int> intItems = new List<int>()
+            {
+                5, 7, 12, 24, 48
+            };
+
+            IEnumerable<string> stringItems = new List<string>()
+            {
+                "Hello", "world", "!", "I", "am", "a", "list", "of", "strings"
+            };
+
+            int intSingle = 96;
+            string stringSingle = ".";
+
+            IEnumerable<int> mergedInts = ListItemsOfType<int>(
+                intItems, intSingle);
             
+            IEnumerable<string> mergedStrings = ListItemsOfType<string>(
+                stringItems, stringSingle);
+
+            Console.WriteLine("List of ints");
+            foreach (int i in mergedInts)
+                Console.WriteLine($"- {i}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("List of strings");
+            foreach (string s in mergedStrings)
+                Console.WriteLine($"- {s}");
         }
 
         private static IEnumerable<T> ListItemsOfType<T>(
