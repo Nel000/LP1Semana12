be3bc8027a6d6ae5fd1774853a25186164f75bf0
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-06-08 21:08:45 +0100

    Initialize Guard3 instances with new values

--- JustLikeACollection/Program.cs
+++ JustLikeACollection/Program.cs
@@ -6,7 +6,25 @@ namespace JustLikeACollection
     {
         static void Main(string[] args)
         {
-            
+            Guarda3<string> valString = new Guarda3<string>() 
+            { 
+                "Hello",
+                "World",
+                "!",
+            };
+
+            Guarda3<float> valFloat = new Guarda3<float>()
+            {
+                27.5f,
+                33.2f,
+                51.4f
+            };
+
+            foreach (string s in valString)
+                Console.WriteLine(s);
+
+            foreach (float f in valFloat)
+                Console.WriteLine(f);
         }
     }
 }
