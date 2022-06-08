b9bec5a8ef1fe1ff448b1d8414a7075e2ae8cc5c
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-06-08 19:08:57 +0100

    Add Get and Set methods

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -7,13 +7,29 @@ namespace MyGenericClass
 {
     public class Guarda3<T>
     {
-        private T val1, val2, val3;
+        private T[] val = new T[] {val1, val2, val3};
 
         public Guarda3()
         {
             val1 = default;
             val2 = default;
             val3 = default;
+        }
+
+        public T GetItem(int i)
+        {
+            if (i < 0 || i > 2)
+                throw new IndexOutOfRangeException();
+
+            return val[i];
+        }
+
+        public void SetItem(int i, T item)
+        {
+            if (i < 0 || i > 2)
+                throw new IndexOutOfRangeException();
+
+            val[i] = item;
         }
     }
 }
