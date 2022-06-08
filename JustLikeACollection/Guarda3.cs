2725aa038c353852eac4202eb82f722a79e4b638
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-06-08 20:45:24 +0100

    Create Add method

---
+++ JustLikeACollection/Guarda3.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace JustLikeACollection
+{
+    public class Guarda3<T> : IEnumerable<T>
+    {
+        private T[] val = new T[3];
+
+        public Guarda3()
+        {
+            for (int i = 0; i < val.Length; i++)
+                val[i] = default;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < val.Length; i++)
+                yield return val[i];
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            bool itemAdded;
+
+            for (int i = 0; i < val.Length; i++)
+                if (val[i] != default && !itemAdded)
+                {
+                    val[i] = item;
+                    itemAdded = true;
+                }
+        }
+
+        public T GetItem(int i)
+        {
+            if (i < 0 || i > 2)
+                throw new IndexOutOfRangeException();
+
+            return val[i];
+        }
+
+        public void SetItem(int i, T item)
+        {
+            if (i < 0 || i > 2)
+                throw new IndexOutOfRangeException();
+
+            val[i] = item;
+        }
+    }
+}
