50f6dcc32e648d85b92f5936a5bad3e2511170af
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-06-08 19:02:02 +0100

    Add variables of generic type and initialize them

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGenericClass
+{
+    public class Guarda3<T>
+    {
+        private T val1, val2, val3;
+
+        public Guarda3()
+        {
+            val1 = default;
+            val2 = default;
+            val3 = default;
+        }
+    }
+}
